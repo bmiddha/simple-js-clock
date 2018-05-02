a62793ea6c4efe0c9e2126f279fbc4e0a413874f
Bharat Middha <bmiddha99@gmail.com>
2018-05-02 09:47:59 -0500

    new typeface code cleanup weather now has "REAL" units aka "metric"

--- main.js
+++ main.js
@@ -1,41 +1,43 @@
 var colors = ["#303030", "#01579B","#006064","#304FFE","#004D40"];
 var images = ["http://flourishconf.com/2019/assets/images/tagline.png","http://flourishconf.com/2019/assets/images/logo.png","https://acm.cs.uic.edu/wiki/_media/logos:website-logo-replacement-2017.png", "https://lug.cs.uic.edu/LUG_logo_Non-UIC.png"]
 var city = 12778674;
+var counter = 0;
+
 function getWeather(){
-    var xhr = new XMLHttpRequest();
-    var url = "https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20%3D%20"+city+"&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+    let xhr = new XMLHttpRequest();
+    let url = "https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20%3D%20"+city+"&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
     xhr.open("GET",url,true);
     xhr.send(); 
     
     xhr.onreadystatechange = function(){
         if(xhr.readyState == 4 && xhr.status == 200){ 
-            var w = JSON.parse(xhr.responseText);
-            document.querySelector("#weather").innerHTML = w.query.results.channel.item.condition.temp + "&#176;F<br>" +  w.query.results.channel.item.condition.text;
-        };
+            let w = JSON.parse(xhr.responseText);
+            let temp = w.query.results.channel.item.condition.temp;
+            let text = w.query.results.channel.item.condition.text;
+            let metric = Math.round((temp - 32) * (5/9));
+            document.querySelector("#weather").innerHTML = text + "<br>Feels Like<br>" + temp + "&#176;F|" + metric + "&#176;C";
+        }
     }
-    setTimeout(getWeather,1000000)
+    setTimeout(getWeather,100000);
 }
 
 
 function startTime() {
-    var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-    var today = new Date();
-    var hour = today.getHours();
-    var min = today.getMinutes();
-    var sec = today.getSeconds();
-    var date = today.getDate();
-    var month = today.getMonth() + 1;
-    var year = today.getFullYear();
-    var day = days[today.getDay()];
-    if (hour >= 12){
-        hour = hour - 12;
-    }
+    let days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    let today = new Date();
+    let hour = today.getHours();
+    let min = today.getMinutes();
+    let sec = today.getSeconds();
+    let date = today.getDate();
+    let month = today.getMonth() + 1;
+    let year = today.getFullYear();
+    let day = days[today.getDay()];
+    hour = (hour >= 12) ? (hour - 12) : hour;
     min = addZero(min);
     sec = addZero(sec);
     hour = addZero(hour);
     date = addZero(date);
     month = addZero(month);
-    // ampm = (hour >= 12) ? "PM" : "AM";
     document.getElementById('time').innerHTML = hour + ":" + min + ":" + sec;
     document.getElementById('date').innerHTML = day + "," + month + "/" + date + "/" + year;
     setTimeout(startTime, 500);
@@ -44,10 +46,6 @@ function addZero(i) {
     if (i < 10) {i = "0" + i};
     return i;
 }
-
-
-
-var counter = 0;
 
 function fadeIn(el){
     el.style.opacity = 0;

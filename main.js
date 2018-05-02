0a0a9d90550b0e26bde411795220b8522f54958d
Bharat Middha <bmiddha99@gmail.com>
2018-05-01 19:33:44 -0500

    added weather new colors 12 hour time

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
-var colors = ["#00293C", "#626D71", "#282655", "#662E1C", "#4C3F54", "#231B12", "#301B28", "#563E20"]
+var colors = ["#303030", "#01579B","#006064","#304FFE","#004D40"];
 var images = ["http://flourishconf.com/2019/assets/images/tagline.png","http://flourishconf.com/2019/assets/images/logo.png","https://acm.cs.uic.edu/wiki/_media/logos:website-logo-replacement-2017.png", "https://lug.cs.uic.edu/LUG_logo_Non-UIC.png"]
+var city = 12778674;
+function getWeather(){
+    var xhr = new XMLHttpRequest();
+    var url = "https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20%3D%20"+city+"&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+    xhr.open("GET",url,true);
+    xhr.send(); 
+    
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4 && xhr.status == 200){ 
+            var w = JSON.parse(xhr.responseText);
+            document.querySelector("#weather").innerHTML = w.query.results.channel.item.condition.temp + "&#176;F<br>" +  w.query.results.channel.item.condition.text;
+        };
+    }
+}
 
 
 function startTime() {
@@ -12,11 +26,15 @@ function startTime() {
     var month = today.getMonth() + 1;
     var year = today.getFullYear();
     var day = days[today.getDay()];
+    if (hour >= 12){
+        hour = hour - 12;
+    }
     min = addZero(min);
     sec = addZero(sec);
     hour = addZero(hour);
     date = addZero(date);
     month = addZero(month);
+    // ampm = (hour >= 12) ? "PM" : "AM";
     document.getElementById('time').innerHTML = hour + ":" + min + ":" + sec;
     document.getElementById('date').innerHTML = day + "," + month + "/" + date + "/" + year;
     var timeFunction = setTimeout(startTime, 500);
@@ -59,3 +77,4 @@ function colorChanger(){
 
 cycleImages();
 colorChanger();
+getWeather();

e13e5041d0e518175ccbb964d7806037bda8101b
Bharat Middha <bmiddha99@gmail.com>
2018-05-23 18:53:26 -0500

    added eslint new node service for cta and weather

--- main.js
+++ main.js
@@ -1,81 +1,94 @@
 var colors = ["#303030", "#01579B","#006064","#304FFE","#004D40"];
-var images = ["http://flourishconf.com/2019/assets/images/tagline.png","http://flourishconf.com/2019/assets/images/logo.png","https://acm.cs.uic.edu/wiki/_media/logos:website-logo-replacement-2017.png", "https://lug.cs.uic.edu/LUG_logo_Non-UIC.png"]
+var images = ["http://flourishconf.com/2019/assets/images/tagline.png","http://flourishconf.com/2019/assets/images/logo.png","https://acm.cs.uic.edu/wiki/_media/logos:website-logo-replacement-2017.png", "https://lug.cs.uic.edu/LUG_logo_Non-UIC.png"];
 var city = 12778674;
 var counter = 0;
 
-function getWeather(){
-    let xhr = new XMLHttpRequest();
-    let url = "https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20%3D%20"+city+"&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
-    xhr.open("GET",url,true);
-    xhr.send(); 
-    
-    xhr.onreadystatechange = function(){
-        if(xhr.readyState == 4 && xhr.status == 200){ 
-            let w = JSON.parse(xhr.responseText);
-            let temp = w.query.results.channel.item.condition.temp;
-            let text = w.query.results.channel.item.condition.text;
-            let metric = Math.round((temp - 32) * (5/9));
-            let kelvin = metric + 273;
-            document.querySelector("#weather-condition").innerHTML = text
-            document.querySelector("#weather-temperature").innerHTML = "Feels Like<br>" + temp + "&#176;F|" + metric + "&#176;C|" + kelvin + "K";
-        }
-    }
-    setTimeout(getWeather,100000);
+function getApiData(){
+	let xhr = new XMLHttpRequest();
+	let url = "http://localhost:8080/api/all?bus=6700,6627,307,332,4640,14487,6347,206&train=40350&city=chicago";
+
+	xhr.open("GET",url,true);
+	xhr.send(); 
+	let busData = [];
+	let trainData = [];
+	let weatherData = [];
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState == 4 && xhr.status == 200){ 
+			let data = JSON.parse(xhr.responseText);
+			busData = data.Bus;
+			trainData = data.Train;
+			weatherData = data.Weather;
+			document.querySelector("#bus").innerHTML = "";
+			for(var i=0;i<busData.length;i++){
+				// console.log(busData[i][0].tmstmp);
+				let timeNow = new Date();
+				let eta = Math.abs((busData[i][0].prdtm.replace(":","")) - (addZero(timeNow.getHours()) +""+ addZero(timeNow.getMinutes())));
+				console.log();
+				document.querySelector("#bus").innerHTML += busData[i][0].route + " " + busData[i][0].dir + " in " + eta + " min<br>";
+			}
+            
+			document.querySelector("#weather-condition").innerHTML = weatherData[0][0].forecast.main[0];
+			document.querySelector("#weather-temperature").innerHTML = "Feels Like<br>" + weatherData[0][0].temp + "K";
+		}
+	};
+	setTimeout(getApiData,15000);
 }
 
 
 function startTime() {
-    let days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-    let today = new Date();
-    let hour = today.getHours();
-    let min = today.getMinutes();
-    let sec = today.getSeconds();
-    let date = today.getDate();
-    let month = today.getMonth() + 1;
-    let year = today.getFullYear();
-    let day = days[today.getDay()];
-    hour = (hour > 12) ? (hour - 12) : hour;
-    min = addZero(min);
-    sec = addZero(sec);
-    hour = addZero(hour);
-    date = addZero(date);
-    month = addZero(month);
-    document.getElementById('time').innerHTML = hour + ":" + min + ":" + sec;
-    document.getElementById('date').innerHTML = day + "," + month + "/" + date + "/" + year;
-    setTimeout(startTime, 500);
+	let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+	let today = new Date();
+	let hour = today.getHours();
+	let min = today.getMinutes();
+	let sec = today.getSeconds();
+	let date = today.getDate();
+	let month = today.getMonth() + 1;
+	let year = today.getFullYear();
+	let day = days[today.getDay()];
+	hour = (hour > 12) ? (hour - 12) : hour;
+	min = addZero(min);
+	sec = addZero(sec);
+	hour = addZero(hour);
+	date = addZero(date);
+	month = addZero(month);
+	document.getElementById("time").innerHTML = hour + ":" + min + ":" + sec;
+	document.getElementById("date").innerHTML = day + "," + month + "/" + date + "/" + year;
+	setTimeout(startTime, 500);
 }
 function addZero(i) {
-    if (i < 10) {i = "0" + i};
-    return i;
+	if (i < 10) {i = "0" + i;}
+	return i;
 }
 
-function fadeIn(el){
-    el.style.opacity = 0;
-    (function fade() {
-        var val = parseFloat(el.style.opacity);
-        if (!((val += .1) > 1)) {
-            el.style.opacity = val;
-            setTimeout(fade, 40);
-        } 
-    })();
-}
+// function fadeIn(el){
+//     el.style.opacity = 0;
+//     (function fade() {
+//         var val = parseFloat(el.style.opacity);
+//         if (!((val += .1) > 1)) {
+//             el.style.opacity = val;
+//             setTimeout(fade, 40);
+//         } 
+//     })();
+// }
 
 function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
+	return Math.floor(Math.random() * Math.floor(max));
 }
 
-function cycleImages() {
-    fadeIn(document.querySelector("#message>img"));
-    document.querySelector("#message>img").src = images[counter%images.length];
-    counter++;
-    setTimeout(cycleImages,20000)
-}
+// function cycleImages() {
+//     fadeIn(document.querySelector("#message>img"));
+//     document.querySelector("#message>img").src = images[counter%images.length];
+//     counter++;
+//     setTimeout(cycleImages,20000)
+// }
 
 function colorChanger(){
-    document.body.style.backgroundColor = colors[getRandomInt(colors.length)];
-    setTimeout(colorChanger, 10000);
+	document.body.style.backgroundColor = colors[getRandomInt(colors.length)];
+	setTimeout(colorChanger, 10000);
 }
 
-cycleImages();
+// cycleImages();
 colorChanger();
-getWeather();
+getApiData();
+
+

6d65e3fe9c99ffcfce2e6c76a7687afd60039df1
Bharat Middha <bmiddha99@gmail.com>
2018-05-24 15:04:35 -0500

    server.js more efficient

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ require("dotenv").config();
 const http = require("http");
 const express = require("express");
 const path = require("path");
-
 const trainApiKey = process.env.CTA_TRAIN_API_KEY;
 const busApiKey = process.env.CTA_BUS_API_KEY;
 const openWeatherApiKey = process.env.OPEN_WEATHER_MAP_API_KEY;
+const app = express();
+const port = 8080;
+app.listen(port);
+console.log("Server started on port " + port);
 
 function getFromUrl(url) {
-	return new Promise( (resolve, reject) => {
+	return new Promise((resolve, reject) => {
 		http.request(url, (response) => {
 			var str = "";
 			response.on("data", (chunk) => {
@@ -19,172 +22,67 @@ function getFromUrl(url) {
 			});
 		}).end();
 	});
-
 }
 
-const busKey = "Bus";
-const trainKey = "Train";
-const weatherKey = "Weather";
-const o = {};
-o[busKey] = [];
-o[trainKey] = [];
-o[weatherKey] = [];
-
-let busStops = [ "6700" , "6627", "307", "332", "4640", "14487", "6347", "206"];
-let trainStations = ["40350"];
-let location = "Chicago";
-
-function getBus() {
-	for (let i=0;i<busStops.length;i++) {
-		let busUrl="http://ctabustracker.com/bustime/api/v2/getpredictions?key="+busApiKey+"&stpid="+busStops[i]+"&format=json";
-		let busRoute = [];
-		let busStopName = [];
-		let busDirection = [];
-		let busPredictedTime = [];
-		let busTimeStamp = [];
-		let busExport = [];
+function getCtaBus(busStop) {
+	return new Promise((resolve, reject) => {
+		let busUrl = "http://ctabustracker.com/bustime/api/v2/getpredictions?key=" + busApiKey + "&stpid=" + busStop + "&format=json";
 		getFromUrl(busUrl).then((result) => {
 			let busJson = JSON.parse(result)["bustime-response"];
 			if (busJson.hasOwnProperty("error")) {
-				// TODO
-				console.log("Error response on bus " + busStops[i]);
-				return;
+				resolve("Error response on bus stop " + busStop);
 			}
-			for (let j=0;j<busJson["prd"].length;j++) {
-				busRoute[j] = busJson["prd"][j]["rt"];
-				busStopName[j] = busJson["prd"][j]["stpnm"];
-				busDirection[j] = busJson["prd"][j]["rtdir"]; 
-				busPredictedTime[j] = busJson["prd"][j]["prdtm"].split(" ")[1];
-				busTimeStamp[j] = busJson["prd"][j]["tmstmp"].split(" ")[1];
-				let data = {
-					stop: busStopName[j],
-					route: busRoute[j],
-					dir: busDirection[j],
-					prdtm: busPredictedTime[j],
-					tmstmp: busTimeStamp[j],
-				};
-				busExport.push(data);
-			}
-			o[busKey].push(busExport);
-        
+			resolve(busJson);
 		});
-	}
+	});
 }
 
-function getTrain() {
-	for (let i=0;i<trainStations.length;i++) {    
-		let trainUrl="http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key="+trainApiKey+"&mapid="+trainStations[i]+"&max=5&outputType=JSON";
-		let trainExport = [];  
+function getCtaTrain(trainStation) {
+	return new Promise((resolve, reject) => {
+		let trainUrl = "http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=" + trainApiKey + "&mapid=" + trainStation + "&max=2&outputType=JSON";
 		getFromUrl(trainUrl).then((result) => {
 			let trainJson = JSON.parse(result);
-			let trainRoute = [];
-			let trainStationName = [];
-			let trainDestination = [];
-			let trainTimeStamp = [];
-			let trainPrdictedTime = [];
-			for (i=0;i<trainJson["ctatt"]["eta"].length;i++) {
-				trainRoute[i] = trainJson["ctatt"]["eta"][i]["rt"];
-				trainStationName[i] = trainJson["ctatt"]["eta"][i]["staNm"];
-				trainDestination[i] = trainJson["ctatt"]["eta"][i]["stpDe"].split(" ")[2];
-				trainTimeStamp[i] = trainJson["ctatt"]["eta"][i]["prdt"].split("T")[1];
-				trainPrdictedTime[i] = trainJson["ctatt"]["eta"][i]["arrT"].split("T")[1];
-				let data = {
-					stop: trainStationName[i],
-					route: trainRoute[i],
-					dir: trainDestination[i],
-					prdtm: trainPrdictedTime[i],
-					tmstmp: trainTimeStamp[i],
-				};
-				trainExport.push(data);
+			if (trainJson.hasOwnProperty("error")) {
+				resolve("Error response on train station " + trainStation);
 			}
-			o[trainKey].push(trainExport);
+			resolve(trainJson);
 		});
-	}
-}
-
-function getWeather() {
-	var weatherUrl="http://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+openWeatherApiKey;
-	var weatherExport = [];
-	getFromUrl(weatherUrl).then((result) => {
-		let weatherJson = JSON.parse(result);
-		let weatherCity = weatherJson["name"] + ", " + weatherJson["sys"]["country"];
-		let weatherWind = weatherJson["wind"];
-		let weatherTemp = weatherJson["main"]["temp"];
-		let weatherForecast = [];
-		let weatherForecastDescription = [];
-		for (let i=0; i<weatherJson["weather"].length;i++) {
-			weatherForecast[i] = weatherJson["weather"][i]["main"];
-			weatherForecastDescription[i] = weatherJson["weather"][i]["description"];
-		}
-		let data = {
-			location: weatherCity,
-			temp: weatherTemp,
-			wind: weatherWind,
-			forecast: {
-				main: weatherForecast,
-				description: weatherForecastDescription,
-			},
-		};
-		weatherExport.push(data);
 	});
-	o[weatherKey].push(weatherExport);
 }
 
-const app = express();
-const port = 8080;
-
-app.listen(port);
-
-function getData() {
-	console.log("getting new data");
-	getBus();
-	getTrain();
-	getWeather();
-	setTimeout(() => {
-		o[busKey] = [];
-		o[trainKey] = [];
-		o[weatherKey] = [];
-		getData();
-	}, 10000);
+function getWeather(city) {
+	return new Promise((resolve, reject) => {
+		var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + openWeatherApiKey;
+		getFromUrl(weatherUrl).then((result) => {
+			let weatherJson = JSON.parse(result);
+			resolve(weatherJson);
+		});
+	});
 }
 
-getData();
-
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 });
 
-app.get("/api/all", function(req, res) {
+app.get("/api/bus", (req, res) => {
 	let bus = req.query.bus;
+	getCtaBus(bus).then((result) => {
+		res.send(result);
+	});
+});
+app.get("/api/train", (req, res) => {
 	let train = req.query.train;
+	getCtaTrain(train).then((result) => {
+		res.send(result);
+	});
+});
+app.get("/api/weather", (req, res) => {
 	let city = req.query.city;
-	busStops = bus.split(",");
-	trainStations = train.split(",") ;
-	location = city;
-	res.send(o);
-});
-
-app.get("/api/bus", function(req, res) {
-	let bus = req.query.bus;
-	busStops = bus.split(",");
-	res.send(o[busKey]);
-});
-app.get("/api/train", function(req, res) {
-	let train = req.query.train;
-	trainStations = train.split(",");
-	res.send(o[trainKey]);
-});
-app.get("/api/weather", function(req, res) {
-	let city = req.query.city;
-	location = city;
-	res.send(o[weatherKey]);
+	getWeather(city).then((result) => {
+		res.send(result);
+	});
 });
 
 app.use(express.static(path.join(__dirname + "/site")));
-
-// bus     : http://localhost:8080/api/bus?bus=6700,6627,307,332,4640,14487,6347,206
-// train   : http://localhost:8080/api/train?train=40350
-// weather : http://localhost:8080/api/weather?city=chicago
-// all     : http://localhost:8080/api/all?bus=6700,6627,307,332,4640,14487,6347,206&train=40350&city=chicago
--- site/main.js
+++ site/main.js
@@ -1,43 +1,22 @@
 const colors = ["#303030", "#01579B","#006064","#304FFE","#004D40"];
-const images = ["http://flourishconf.com/2019/assets/images/tagline.png","http://flourishconf.com/2019/assets/images/logo.png","https://acm.cs.uic.edu/wiki/_media/logos:website-logo-replacement-2017.png", "https://lug.cs.uic.edu/LUG_logo_Non-UIC.png"];
-const city = 12778674;
-let counter = 0;
+const busStops = [ "6700" , "6627", "307", "332", "4640", "14487", "6347", "206"];
+const trainStations = ["40350"];
+const city = "Chicago";
 
-function getApiData(){
-	let xhr = new XMLHttpRequest();
-	let url = "http://localhost:8080/api/all?bus=6700,6627,307,332,4640,14487,6347,206&train=40350&city=chicago";
-
-	xhr.open("GET",url,true);
-	xhr.send(); 
-	let busData = [];
-	let trainData = [];
-	let weatherData = [];
-	xhr.onreadystatechange = function(){
-		if(xhr.readyState == 4 && xhr.status == 200){ 
-			let data = JSON.parse(xhr.responseText);
-			busData = data.Bus;
-			trainData = data.Train;
-			weatherData = data.Weather;
-			document.querySelector("#bus").innerHTML = "";
-			for(let i=0;i<busData.length;i++){
-				// console.log(busData[i][0].tmstmp);
-				let timeNow = new Date();
-				let eta;
-				for(let j=0;j<busData[i].length;j++)
-				{
-					eta = Math.abs((busData[i][j].prdtm.replace(":","")) - (addZero(timeNow.getHours()) +""+ addZero(timeNow.getMinutes())));
-					document.querySelector("#bus").innerHTML += busData[i][j].route + " " + busData[i][j].dir + " in " + eta + " min<br>";
-				}
-				// console.log(eta);
+function getApiData(type,arg,value) {
+	return new Promise ((resolve,reject) => {
+		let xhr = new XMLHttpRequest();
+		let url = "http://localhost:8080/api/"+type+"/?"+arg+"="+value;
+		xhr.open("GET",url,true);
+		xhr.send();
+		xhr.onreadystatechange = function(){
+			if(xhr.readyState == 4 && xhr.status == 200){ 
+				let data = JSON.parse(xhr.responseText);
+				resolve (data);
 			}
-            
-			document.querySelector("#weather-condition").innerHTML = weatherData[0][0].forecast.main[0];
-			document.querySelector("#weather-temperature").innerHTML = "Feels Like<br>" + weatherData[0][0].temp + "K";
-		}
-	};
-	setTimeout(getApiData,3000);
+		};
+	});
 }
-
 
 function startTime() {
 	let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
@@ -64,35 +43,33 @@ function addZero(i) {
 	return i;
 }
 
-// function fadeIn(el){
-//     el.style.opacity = 0;
-//     (function fade() {
-//         var val = parseFloat(el.style.opacity);
-//         if (!((val += .1) > 1)) {
-//             el.style.opacity = val;
-//             setTimeout(fade, 40);
-//         } 
-//     })();
-// }
 
 function getRandomInt(max) {
 	return Math.floor(Math.random() * Math.floor(max));
 }
-
-// function cycleImages() {
-//     fadeIn(document.querySelector("#message>img"));
-//     document.querySelector("#message>img").src = images[counter%images.length];
-//     counter++;
-//     setTimeout(cycleImages,20000)
-// }
 
 function colorChanger(){
 	document.body.style.backgroundColor = colors[getRandomInt(colors.length)];
 	setTimeout(colorChanger, 10000);
 }
 
-// cycleImages();
+function getData() {
+	console.clear();
+	for (let i = 0; i < busStops.length;i++) {
+		let data = getApiData("bus","bus",busStops[i]).then((result)=>{
+			console.log("BUS STOP: " + busStops[i], result);
+		});
+	}
+	for (let i = 0; i < trainStations.length;i++) {
+		let data = getApiData("train","train",trainStations[i]).then((result)=>{
+			console.log("TRAIN STATION: " + trainStations[i], result);
+		});
+	}
+	let data = getApiData("weather","city",city).then((result)=>{
+		console.log("WEATHER: " + city, result);
+	});
+	setTimeout(getData,60000);
+}
+
 colorChanger();
-getApiData();
-
-
+getData();
